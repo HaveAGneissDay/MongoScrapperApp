80e85962c08046c5052e0a6fc91892caa583f971
David Cheng <dkcheng10@gmail.com>
2018-03-22 11:26:54 -0700

    added middleware and api rotues

--- server.js
+++ server.js
@@ -8,6 +8,26 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+// Serve static content for the app from the 'public' directory
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set Handlebars as the view engine
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Import routes and give the server access to them
+var routes = require('./routes/api/apiroutes.js');
+
+app.use('/', routes);
+
+
+
+
 //Make sure the app is listening
 app.listen(PORT, function() {
     console.log("App is listening on PORT " + PORT);
