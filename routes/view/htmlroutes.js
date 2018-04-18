0d2d1af61ba7d46d00bb08089cf129a12a221fb7
David Cheng <dkcheng10@gmail.com>
2018-04-17 17:38:03 -0700

    added html routes deleted index.js(not needed/ used)

--- routes/view/htmlroutes.js
+++ routes/view/htmlroutes.js
@@ -1,8 +1,23 @@
 //This file puts everything into view
 //Need packages
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 //we combined article and note into one file
 const Combined = require('../models/index.js');
 
 const scraper = require('../scripts/scrape.js');
+
+module.exports = function (app) {
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "views/index.html"));
+    });
+
+    app.get("/saved/all", function (req, res) {
+        res.sendFile(path.join(__dirname, "views/saved.html"));
+    });
+
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "views/index.html"));
+    });
+}
